8c0ca1e17356c324eba168fc6c986ba996bedfac
zendo <jzendo@example.com>
2020-03-02 16:31:10 +0800

    Add (deep) freeze object

---
+++ packages/utils/src/object/__tests__/freeze.js
@@ -0,0 +1,81 @@
+/* global describe, test, expect */
+import freeze from '../freeze'
+
+describe('object freeze', () => {
+  test('default freezen', () => {
+    let a = {
+      test: 1
+    }
+    let freezenA = freeze(a)
+
+    expect(Object.isFrozen(a)).toBeTruthy()
+    expect(freezenA).toEqual(a)
+
+    // Can not add new property
+    expect(() => {
+      a.testNewProp = 123
+    }).toThrow()
+
+    // can not delete property
+    expect(() => {
+      delete a.test
+    }).toThrow()
+
+    // can not modify property-value
+    expect(() => {
+      a.test = 123
+    }).toThrow()
+  })
+
+  test('shallow freezen', () => {
+    let a = {
+      test: 1,
+      test2: {
+        // can add/remove/modify object
+        test2Child: 2
+      }
+    }
+    let freezenA = freeze(a, true)
+
+    expect(Object.isFrozen(a)).toBeTruthy()
+    expect(freezenA).toEqual(a)
+
+    // Can not add
+    expect(() => {
+      a.test2.testNewProp = 123
+    }).not.toThrow()
+
+    // can modify
+    expect(() => {
+      a.test2.testNewProp = 567
+    }).not.toThrow()
+
+    expect(a.test2.testNewProp).toEqual(567)
+
+    // can not delete
+    expect(() => {
+      delete a.test2.testNewProp
+    }).not.toThrow()
+
+    expect(a.test2.testNewProp).toBeUndefined()
+  })
+
+  test('prototype property freezen', () => {
+    class A {
+      get test() {
+        return 'test'
+      }
+    }
+
+    const a = new A
+    const freezenA = freeze(a)
+
+    expect(Object.isFrozen(a)).toBeTruthy()
+    expect(freezenA).toEqual(a)
+
+    // Can not modify prototype property which has be freezen
+    expect(() => {
+      a.__proto__.test = 567
+    }).toThrow()
+  })
+})
---
+++ packages/utils/src/object/freeze.js
@@ -0,0 +1,32 @@
+/**
+ * Get the freezen object.
+ * Inflenced by https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+ *
+ * ```
+ *   A frozen object can no longer be changed; freezing an object prevents new properties from being added to it,
+ *   existing properties from being removed, prevents changing the enumerability, configurability,
+ *   or writability of existing properties, and prevents the values of existing properties from being changed.
+ *   In addition, freezing an object also prevents its prototype from being changed.
+ *   freeze() returns the same object that was passed in.
+ * ```
+ *
+ * NOTE: As `onlyShallowFreeze = false` is default,
+ *       but you use `onlyShallowFreeze = true` as possible as can for performance
+ * @param {Object} object the freezing object
+ * @param {boolean} onlyShallowFreeze whether shallow freeze or not
+ * @returns {Object} the freezen object
+ */
+export default function deepFreeze(object, onlyShallowFreeze = false) {
+  if (!onlyShallowFreeze) {
+    // Retrieve the property names defined on object
+    var propNames = Object.getOwnPropertyNames(object)
+    for (let name of propNames) {
+      let value = object[name]
+      if(value && typeof value === "object") {
+        deepFreeze(value)
+      }
+    }
+  }
+
+  return Object.freeze(object)
+}
